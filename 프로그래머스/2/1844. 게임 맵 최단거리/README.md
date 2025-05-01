fa4741b40f56c69fa8113f86dbea7b9064f3c227
k9dong <144599580+k9dong@users.noreply.github.com>
2025-05-01 12:42:51 +0900

    [level 2] Title: 게임 맵 최단거리, Time: 8.79 ms, Memory: 54.9 MB -BaekjoonHub

--- 프로그래머스/2/1844. 게임 맵 최단거리/README.md
+++ 프로그래머스/2/1844. 게임 맵 최단거리/README.md
@@ -1,10 +1,10 @@
 # [level 2] 게임 맵 최단거리 - 1844 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/1844?language=java) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/1844) 
 
 ### 성능 요약
 
-메모리: 55.7 MB, 시간: 17.64 ms
+메모리: 54.9 MB, 시간: 8.79 ms
 
 ### 구분
 
@@ -16,7 +16,7 @@
 
 ### 제출 일자
 
-2025년 04월 08일 22:56:18
+2025년 05월 01일 12:42:48
 
 ### 문제 설명
 
--- 프로그래머스/2/1844. 게임 맵 최단거리/게임 맵 최단거리.java
+++ 프로그래머스/2/1844. 게임 맵 최단거리/게임 맵 최단거리.java
@@ -2,16 +2,20 @@ import java.util.*;
 
 class Solution {
     public int solution(int[][] maps) {
-        int height = maps.length;
-        int width = maps[0].length;
+        int height = maps.length-1;
+        int width = maps[0].length-1;
 
         // 방향: 하, 상, 우, 좌
         int[] dx = {1, -1, 0, 0};
         int[] dy = {0, 0, 1, -1};
 
+        //Queue<int[]> queue = new LinkedList<>();
+        //queue.offer(new int[]{0, 0});
+        
         Queue<int[]> queue = new LinkedList<>();
-        queue.offer(new int[]{0, 0});
-
+        queue.offer(new int[]{0,0});
+        
+        
         while (!queue.isEmpty()) {
             int[] now = queue.poll();
             int x = now[0];
@@ -20,23 +24,22 @@ class Solution {
             for (int i = 0; i < 4; i++) {
                 int nx = x + dx[i];
                 int ny = y + dy[i];
-
-                // 범위 체크
-                if (nx < 0 || ny < 0 || nx >= height || ny >= width)
+                
+                if (nx > height || ny > width || nx < 0 || ny <0){
                     continue;
-
-                // 벽이거나 이미 방문한 경우
-                if (maps[nx][ny] != 1)
+                }
+                
+                if (maps[nx][ny] !=1){
                     continue;
-
-                // 거리 갱신 및 큐에 추가
+                }
+                
                 maps[nx][ny] = maps[x][y] + 1;
-                queue.offer(new int[]{nx, ny});
+                queue.offer(new int[]{nx,ny});
             }
         }
 
         // 도착지 값이 여전히 1이면 도달 못한 것
-        int answer = maps[height - 1][width - 1];
+        int answer = maps[height][width];
         return answer == 1 ? -1 : answer;
     }
 }
