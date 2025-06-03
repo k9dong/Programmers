72e5b89f9448f865565da4af11fcd1ad34bb10f8
k9dong <144599580+k9dong@users.noreply.github.com>
2025-06-03 21:31:55 +0900

    [level 1] Title: 과일 장수, Time: 402.72 ms, Memory: 136 MB -BaekjoonHub

--- 프로그래머스/1/135808. 과일 장수/과일 장수.java
+++ 프로그래머스/1/135808. 과일 장수/과일 장수.java
@@ -1,25 +1,19 @@
 import java.util.*;
 class Solution {
     public int solution(int k, int m, int[] score) {
-        int answer = 0;
-        Integer[] boxed = Arrays.stream(score).boxed().toArray(Integer[]::new);
-        Arrays.sort(boxed, Collections.reverseOrder());
-        int p = 0; // 최소가격 
-        int count = score.length;
-        int lee = m;
+        int answer = 0; //제일 낮은 점수가 정가일 낮은 점수가 정가
         
-        for (int i= 0; i < score.length ; i++){
-            
-            if (boxed[i]<=k){
-                p = boxed[i]; // p에는 한상자안의 제일 작은 가격이 담겨있다
-            } lee--; // m 카운트용
-            if (lee ==0){
-                answer += p * m;
-                p = 0;
-                lee = m;
+        PriorityQueue<Integer> maxHeap = new PriorityQueue(Collections.reverseOrder());
+        for (int s : score) maxHeap.add(s);
+        while (maxHeap.size() >= m) {
+            int min = 999;
+        for (int i=0; i<m; i++){
+            int num = maxHeap.poll();
+            if (min > num){
+                min = num;
             }
-            
-        }
+        } answer += min * m;
+            }
         return answer;
     }
 }
