e3f33254c72187d3e2572117971264807bbc0f0c
k9dong <144599580+k9dong@users.noreply.github.com>
2025-09-16 21:30:53 +0900

    [level 2] Title: 무인도 여행, Time: 6.74 ms, Memory: 74.6 MB -BaekjoonHub

--- 프로그래머스/2/154540. 무인도 여행/무인도 여행.java
+++ 프로그래머스/2/154540. 무인도 여행/무인도 여행.java
@@ -1,43 +1,40 @@
 import java.util.*;
 class Solution {
-    static int n;
-    static int m;
-    private static boolean [][] visited;
-    
+    private boolean[][] visited;
+    private int n,m;
     public int[] solution(String[] maps) {
-        List<Integer> arr = new ArrayList<>();
-        n = maps.length; // 행
-        m = maps[0].length(); // 열
-        visited = new boolean [n][m];
-        
+        n = maps.length; //행
+         m = maps[0].length(); //열
+        int[] answer = {};
+        int count=0;
+        List<Integer> result= new ArrayList<>();
+        visited = new boolean[n][m];
+         
         for(int i=0; i<n; i++){
-            for (int j=0; j<m; j++){
-                char c = maps[i].charAt(j);
-                if (!visited[i][j] && c != 'X'){
-                    
-                    int sum = dfs(i, j, maps);
-                    arr.add(sum);
+            for(int j=0; j<m; j++){
+                if (maps[i].charAt(j) != 'X'&& !visited[i][j]){
+                    count = dfs(i,j,maps);
+                    result.add(count);
                 }
             }
         }
-        if(arr.isEmpty()) return new int[]{-1};
-        Collections.sort(arr);
-        return arr.stream().mapToInt(i->i).toArray();
+        if (result.size()<1) return new int[]{-1};
         
-       
+        int [] arr = result.stream().mapToInt(i->i).toArray();
+        Arrays.sort(arr);
+        return arr;
     }
-    private int dfs (int x, int y, String [] maps){
+    private int dfs(int x, int y,String [] maps){
         visited[x][y] = true;
-        int [] dx = {1, -1, 0, 0};
-        int [] dy = {0, 0, 1, -1};
-        int count = maps[x].charAt(y) - '0';
+        int sum = maps[x].charAt(y) - '0';
+        int[] dx = {1,-1,0,0};
+        int[] dy = {0,0,1,-1};
         for (int i=0; i<4; i++){
             int nx = x + dx[i];
             int ny = y + dy[i];
-            if (nx >= 0 && ny >=0 && nx <n && ny <m && !visited[nx][ny] && maps[nx].charAt(ny) != 'X'){
-                count += dfs(nx,ny,maps);
-            }
-        }return count;
+            if(nx>=0 && ny >=0 && n > nx && m > ny&& !visited[nx][ny] && maps[nx].charAt(ny) !='X'){
+               sum += dfs(nx,ny,maps);
+            } 
+        }return sum;
     }
-    
 }
