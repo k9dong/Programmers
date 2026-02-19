3e006129f26ee67171053951b60ea25a58992743
k9dong <144599580+k9dong@users.noreply.github.com>
2026-02-19 16:11:47 +0900

    [level 1] Title: 시저 암호, Time: 1.28 ms, Memory: 85.4 MB -BaekjoonHub

--- 프로그래머스/1/12926. 시저 암호/시저 암호.java
+++ 프로그래머스/1/12926. 시저 암호/시저 암호.java
@@ -3,18 +3,17 @@ class Solution {
     public String solution(String s, int n) {
         String answer = "";
         StringBuilder sb = new StringBuilder();
-        for (char c : s.toCharArray()){
-        if(Character.isUpperCase(c)){
-            c = (char)((c-'A'+n) % 26 +'A');
+        for(char c: s.toCharArray()){
+            if(c==' '){
+                sb.append(c);
+                continue;
+            }
+            if(Character.isUpperCase(c)){
+                c = (char) (((c-'A'+n) %26) +'A');
+            }else{
+                c= (char) (((c-'a'+n)%26) +'a');
+            }
             sb.append(c);
-        }else if(Character.isLowerCase(c)){
-            c = (char) ((c-'a'+n) % 26 +'a');
-            sb.append(c);
-        }else{
-            sb.append(c);
-        }
-        
-
         }
         return sb.toString();
     }
