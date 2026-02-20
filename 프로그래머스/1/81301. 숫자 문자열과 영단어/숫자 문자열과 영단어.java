355de1d19de1839c4fb3cc759d0b9c998f190c2b
k9dong <144599580+k9dong@users.noreply.github.com>
2026-02-20 15:19:29 +0900

    [level 1] Title: 숫자 문자열과 영단어, Time: 0.12 ms, Memory: 89.9 MB -BaekjoonHub

--- 프로그래머스/1/81301. 숫자 문자열과 영단어/숫자 문자열과 영단어.java
+++ 프로그래머스/1/81301. 숫자 문자열과 영단어/숫자 문자열과 영단어.java
@@ -1,13 +1,36 @@
+import java.util.*;
 class Solution {
-    private static final String [] words = {
-        "zero", "one", "two", "three", "four",
-        "five", "six", "seven", "eight", "nine"
-    };
     public int solution(String s) {
         int answer = 0;
-        for (int i=0; i<words.length ; i++){
-            s = s.replace(words[i], Integer.toString(i));
+        StringBuilder sb = new StringBuilder();
+        StringBuilder sb2= new StringBuilder();
+        Map<String,String> map = new HashMap<>();
+        map.put("zero","0");
+        map.put("one","1");
+        map.put("two","2");
+        map.put("three","3");
+        map.put("four","4");
+        map.put("five","5");
+        map.put("six","6");
+        map.put("seven","7");
+        map.put("eight","8");
+        map.put("nine","9");
+        
+        
+        for(char c : s.toCharArray()){
+            if(Character.isDigit(c)){
+                sb2.append(c);
+                sb.setLength(0);
+                continue;
+            }else{
+                sb.append(c);
+                if(map.containsKey(sb.toString())){
+                    String t= map.get(sb.toString());
+                    sb2.append(t);
+                    sb.setLength(0);
+                }
+            }
         }
-        return Integer.parseInt(s);
+        return Integer.parseInt(sb2.toString());
     }
 }
